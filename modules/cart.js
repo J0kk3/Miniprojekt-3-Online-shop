ee37047de113eeb7f6ddd5ff7f4eaa874c9744a3
BennyGRIT <benny.andersson@gritacademy.se>
2022-03-01 10:48:25 +0100

    Creating new Cart

---
+++ modules/cart.js
@@ -0,0 +1,9 @@
+// Functions to handle the cart
+// import { fetchTheShit } from "./fetch.js";
+
+
+function cartFunction(){
+    document.getElementById("hpOmenQ").innerHTML = "3"
+}
+  
+export {cartFunction}
